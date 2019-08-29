0bdb07f5a9b2140c97d0eeeaee9aa0c89677bfb8
Ryosuke <rsasaki@keio.jp>
2019-08-29 18:08:50 +0900

    add pinMode, logging

--- main/test/test.ino
+++ main/test/test.ino
@@ -11,28 +11,35 @@ Servo servo2;
 
 
 void setup() {
+  Serial.begin(115200);
   servo1.attach(0);
   servo2.attach(6);
   servo1.write(0);
   servo2.write(0);
+
+  pinMode(4, INPUT_PULLUP)
 
 }
 
 void loop() {
   if (flag0 == false){
     release_time = millis();
+    Serial.println("ready")
     if (digitalRead(4) == LOW) {
       flag0 == true;
+      Serial.println("released")
     }
     
   } else {
     if ((flag1 == false) && (millis() > release_time + 5000)) {
       servo1.write(180);
       flag1 = true;
+      Serial.println("servo1 rotate")
       
     } else if ((flag2 == false) && (millis() > release_time + 7000)) {
       servo2.write(180);
       flag2 = true;
+      Serial.println("servo2 rotate")
       
     }
     
