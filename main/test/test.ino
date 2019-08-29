96a490f02e4695ce892cc9d7e64cad800084296d
Ryosuke <rsasaki@keio.jp>
2019-08-29 17:57:48 +0900

    flight pin control

---
+++ main/test/test.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+boolean flag0 = false;
+boolean flag1 = false;
+boolean flag2 = false;
+
+unsigned long release_time;
+
+Servo servo1;
+Servo servo2;
+
+
+void setup() {
+  servo1.attach(0);
+  servo2.attach(6);
+  servo1.write(0);
+  servo2.write(0);
+
+}
+
+void loop() {
+  if (flag0 == false){
+    release_time = millis();
+    if (digitalRead(4) == LOW) {
+      flag0 == true;
+    }
+    
+  }
+  if (flag0 == true){
+    if ((flag1 == false) && (millis() > release_time + 5000)) {
+      servo1.write(180);
+      flag1 = true;
+      
+    } else if ((flag2 == false) && (millis() > release_time + 7000)) {
+      servo2.write(180);
+      flag2 = true;
+      
+    }
+    
+  }
+
+}
