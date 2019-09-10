0e3be2fad1f8fa985b7a05af9562fc534d5de59f
Ryosuke <rsasaki@keio.jp>
2019-09-10 17:53:27 +0900

    add main.ino

---
+++ main/main/main.ino
@@ -0,0 +1,93 @@
+#include <SD.h>
+#include <Wire.h>
+
+#define BAUDRATE 9600
+
+#define SD_CS_PIN 10
+
+#define MPU6050_WHO_AM_I     0x75  // Read Only
+#define MPU6050_PWR_MGMT_1   0x6B  // Read and Write
+#define MPU_ADDRESS  0x68
+
+File file;
+
+void setup()
+{
+    Serial.begin(9600);
+    if (!Serial){
+        ;
+    }
+    Serial.println("initializing SD...");
+    if (!SD.begin(SD_CS_PIN)){
+        Serial.println("begin failed");
+    }
+    //file = SD.open("fucking.LOG", FILE_WRITE);
+    char fname[10];
+	for(int i=0;i<=999;i++){
+		sprintf(fname, "%d.LOG", i);
+		if(!SD.exists(fname))
+			break;
+	}
+    file = SD.open(fname, FILE_WRITE);
+
+
+    Wire.begin();
+    // 初回の読み出し
+    Wire.beginTransmission(MPU_ADDRESS);
+    Wire.write(MPU6050_WHO_AM_I);  //MPU6050_PWR_MGMT_1
+    Wire.write(0x00);
+    Wire.endTransmission();
+
+    // 動作モードの読み出し
+    Wire.beginTransmission(MPU_ADDRESS);
+    Wire.write(MPU6050_PWR_MGMT_1);  //MPU6050_PWR_MGMT_1レジスタの設定
+    Wire.write(0x00);
+    Wire.endTransmission();
+
+}
+
+void loop()
+{
+    Wire.beginTransmission(0x68);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(0x68, 14, true);
+    while (Wire.available() < 14);
+    int16_t axRaw, ayRaw, azRaw, gxRaw, gyRaw, gzRaw, Temperature;
+
+    axRaw = Wire.read() << 8 | Wire.read();
+    ayRaw = Wire.read() << 8 | Wire.read();
+    azRaw = Wire.read() << 8 | Wire.read();
+    Temperature = Wire.read() << 8 | Wire.read();
+    gxRaw = Wire.read() << 8 | Wire.read();
+    gyRaw = Wire.read() << 8 | Wire.read();
+    gzRaw = Wire.read() << 8 | Wire.read();
+
+    // 加速度値を分解能で割って加速度(G)に変換する
+    float acc_x = axRaw / 16384.0;  //FS_SEL_0 16,384 LSB / g
+    float acc_y = ayRaw / 16384.0;
+    float acc_z = azRaw / 16384.0;
+
+    // 角速度値を分解能で割って角速度(degrees per sec)に変換する
+    float gyro_x = gxRaw / 131.0;//FS_SEL_0 131 LSB / (°/s)
+    float gyro_y = gyRaw / 131.0;
+    float gyro_z = gzRaw / 131.0;
+
+    file.print(acc_x);  file.print(",");
+    file.print(acc_y);  file.print(",");
+    file.print(acc_z);  file.print(",");
+    file.print(gyro_x); file.print(",");
+    file.print(gyro_y); file.print(",");
+    file.print(gyro_z); file.println("");
+    /**
+    Serial.print(acc_x);  Serial.print(",");
+    Serial.print(acc_y);  Serial.print(",");
+    Serial.print(acc_z);  Serial.print(",");
+    Serial.print(gyro_x); Serial.print(",");
+    Serial.print(gyro_y); Serial.print(",");
+    Serial.print(gyro_z); Serial.println("");
+    **/
+    file.flush();
+    delayMicroseconds(10);
+
+}
